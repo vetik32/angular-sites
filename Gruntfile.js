98317b1a124c1da0cb69582a9cb832a16b4fa3a0
Jeff Cross <middlefloor@gmail.com>
2013-04-24 08:41:28 -0700

    Configured grunt replace task.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,22 +1,21 @@
 module.exports = function (grunt) {
-	grunt.initConfig({
-		pkg: grunt.file.readJSON('package.json'),
-		replace: {
-			dist: {
-				options: {
-					variables: {
-						'pwd' : process.cwd(),
-						'foo' : 'bar'
-					},
-					prefix: '@@'
-				}
-			},
-			files: [{
-				src: ['server/sample/nginx.conf', 'server/sample/sites.conf'], dest: 'server/config/'
-			}]
-		}
-	});
-	grunt.loadNpmTasks('grunt-replace');
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    replace: {
+      dist: {
+        options: {
+          variables: {
+            'pwd': process.cwd()
+          },
+          prefix: '@@'
+        },
+        files: [{
+          flatten: true, expand: true, src: ['server/sample/nginx.conf', 'server/sample/sites.conf'], dest: 'server/config/'
+        }]
+      }
+    }
+  });
+  grunt.loadNpmTasks('grunt-replace');
 
-	grunt.registerTask('configure', ['replace']);
+  grunt.registerTask('default', ['replace']);
 };
