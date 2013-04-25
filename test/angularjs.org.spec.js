2deea028958a499b113ceda9f34f4bf39f978c9f
Jeff Cross <middlefloor@gmail.com>
2013-04-25 10:36:34 -0700

    Added more angularjs.org specs.

--- test/angularjs.org.spec.js
+++ test/angularjs.org.spec.js
@@ -2,13 +2,13 @@ var webdriver = require('selenium-webdriver')
   , expect = require('expect.js')
   , protractor = require('protractor')
   , driver = new webdriver.Builder().
-    usingServer('http://localhost:4444/wd/hub').
-    withCapabilities({
-      'browserName': 'chrome',
-      'version': '',
-      'platform': 'ANY',
-      'javascriptEnabled': true
-    }).build()
+      usingServer('http://localhost:4444/wd/hub').
+      withCapabilities({
+        'browserName': 'chrome',
+        'version': '',
+        'platform': 'ANY',
+        'javascriptEnabled': true
+      }).build()
   , tractor
   , HOST = 'http://angularjs.org';
 
@@ -27,6 +27,7 @@ describe('Angularjs.org', function () {
 
   describe('App', function () {
     tractor.get(HOST);
+
     it('should load the web page', function (done) {
       tractor.findElement(protractor.By.tagName('body')).getAttribute('ng-controller').then(function (controller) {
         expect(controller).to.equal('DownloadCtrl');
@@ -59,28 +60,50 @@ describe('Angularjs.org', function () {
       });
 
       it('should update the Hello text after entering a name', function (done) {
-        // tractor.findElement(protractor.By.input('yourName')).getAttribute('').then(function (text) {
-            // console.log('text', text);
-          
-          // tractor.findElement(protractor.By.binding('yourName')).getText().then(function (text) {
-            // expect(text).to.equal('Hello Jeff!');
-            // done();
-          // });
-        // });
-        done();
-        // });
-        
-        
+        var el = tractor.findElement(protractor.By.input('yourName'));
+        el.click().then(function () {
+          el.sendKeys('Jeff').then(function (type) {
+            var bound = tractor.findElement(protractor.By.css('.container > *:nth-child(4) h1'));
+            bound.getText().then(function (text) {
+              expect(text).to.equal('Hello Jeff!');
+              done();
+            });            
+          });
+        });        
       });
-
-
     });
 
     describe('Add Some Control', function () {
+      it('should strike out a todo when clicked', function (done) {
+        var el = tractor.findElement(protractor.By.css('[ng-controller="TodoCtrl"] ul >li:nth-child(2) input'));
+        el.click().then(function () {
+          var text = tractor.findElement(protractor.By.css('[ng-controller="TodoCtrl"] ul >li:nth-child(2) span'))
+          .getAttribute('class').then(function (c) {
+            expect(c).to.equal('done-true');
+            done();
+          });
+        });
+      });
 
+      it('should add a new todo when added through text field', function (done) {
+        var el = tractor.findElement(protractor.By.input('todoText'));
+        el.click().then(function() {
+          el.sendKeys('Write tests!').then(function () {
+            var btn = tractor.findElement(protractor.By.css('[ng-submit="addTodo()"] .btn-primary'));
+            btn.click().then(function () {
+              var newItem = tractor.findElement(protractor.By.css('[ng-repeat="todo in todos"]:nth-child(3) span'));
+              newItem.getText().then(function (text) {
+                expect(text).to.equal('Write tests!');
+                done();
+              });
+            });
+          });
+        });
+      });
     });
 
     describe('Wire up a Backend', function () {
+      
 
     });
 
