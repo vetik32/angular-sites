f430002192663298f044a2055dec1d39ffc4b1c8
Jeff Cross <middlefloor@gmail.com>
2013-07-01 16:03:27 -0700

    test(docs): Updated docs tests to use protractor runner.

--- test/docs.angularjs.org.spec.js
+++ test/docs.angularjs.org.spec.js
@@ -1,32 +1,12 @@
 describe('docs.angularjs.org', function () {
-  var webdriver = require('selenium-webdriver')
-  , expect = require('expect.js')
-  , protractor = require('protractor')
-  , driver = new webdriver.Builder().
-      usingServer('http://localhost:4444/wd/hub').
-      withCapabilities({
-        'browserName': 'chrome',
-        'version': '',
-        'platform': 'ANY',
-        'javascriptEnabled': true
-      }).build()
-  , tractor
+  var protractor = require('protractor')
+  , tractor = protractor.getInstance()
   , envConfig = require('../server/config/env-config')
   , HOST = envConfig.urls.docs
   , request = require('request')
   , parseXML = require('xml2js').parseString
-  , fs = require('fs');
-
-var catchPromiseErrors = function(done) {
-  webdriver.promise.controlFlow().
-    on('uncaughtException', function(e) {
-        done(e);
-    });
-};
-
-  tractor = protractor.wrapDriver(driver);
-
-  driver.manage().timeouts().setScriptTimeout(10000);
+  , fs = require('fs')
+  , reqTimeout = 5000;
 
   function queryCss (selector) {
     return tractor.findElement(protractor.By.css(selector));
@@ -37,7 +17,7 @@ var catchPromiseErrors = function(done) {
       tractor.get(HOST + '/guide');
       var introParagraph = queryCss('.content p');
       introParagraph.getText().then(function (text) {
-        expect(text).to.contain('Welcome to the angular Developer Guide.');
+        expect(text).toContain('Welcome to the angular Developer Guide.');
         done();
       });
     });
@@ -46,7 +26,7 @@ var catchPromiseErrors = function(done) {
       tractor.get(HOST + '/guide/directive');
       var introParagraph = queryCss('.content p');
       introParagraph.getText().then(function (text) {
-        expect(text).to.contain('Directives are a way to teach HTML new tricks.');
+        expect(text).toContain('Directives are a way to teach HTML new tricks.');
         done();
       });
       
@@ -56,7 +36,7 @@ var catchPromiseErrors = function(done) {
       tractor.get(HOST + '/api');
       var introParagraph = queryCss('.content p');
       introParagraph.getText().then(function (text) {
-        expect(text).to.contain('Use the API Reference documentation');
+        expect(text).toContain('Use the API Reference documentation');
         done();
       });
     });
@@ -65,7 +45,7 @@ var catchPromiseErrors = function(done) {
       tractor.get(HOST + '/api/ng.directive:ngApp');
       var introParagraph = queryCss('.content p');
       introParagraph.getText().then(function (text) {
-        expect(text).to.contain('Use this directive to auto-bootstrap an application.');
+        expect(text).toContain('Use this directive to auto-bootstrap an application.');
         done();
       });
     });
@@ -74,7 +54,7 @@ var catchPromiseErrors = function(done) {
       tractor.get(HOST + '/cookbook');
       var introParagraph = queryCss('.content p');
       introParagraph.getText().then(function (text) {
-        expect(text).to.contain('Welcome to the Angular cookbook');
+        expect(text).toContain('Welcome to the Angular cookbook');
         done();
       });
     });
@@ -83,7 +63,7 @@ var catchPromiseErrors = function(done) {
       tractor.get(HOST + '/cookbook/form');
       var introParagraph = queryCss('.content p');
       introParagraph.getText().then(function (text) {
-        expect(text).to.contain('For this reason Angular strives to make both of these operations trivial.');
+        expect(text).toContain('For this reason Angular strives to make both of these operations trivial.');
         done();
       });
     });
@@ -92,7 +72,7 @@ var catchPromiseErrors = function(done) {
       tractor.get(HOST + '/misc/contribute');
       var introParagraph = queryCss('.content');
       introParagraph.getText().then(function (text) {
-        expect(text).to.contain('AngularJS is an open source project licensed under the');
+        expect(text).toContain('AngularJS is an open source project licensed under the');
         done();
       });
     });
@@ -101,7 +81,7 @@ var catchPromiseErrors = function(done) {
       tractor.get(HOST + '/tutorial');
       var introParagraph = queryCss('.content p');
       introParagraph.getText().then(function (text) {
-        expect(text).to.contain('You will need Node.js');
+        expect(text).toContain('You will need Node.js');
         done();
       });
     });
@@ -110,7 +90,7 @@ var catchPromiseErrors = function(done) {
       tractor.get(HOST + '/tutorial/step_01');
       var introParagraph = queryCss('.content p');
       introParagraph.getText().then(function (text) {
-        expect(text).to.contain('In order to illustrate how Angular enhances standard HTML');
+        expect(text).toContain('In order to illustrate how Angular enhances standard HTML');
         done();
       });
     });
@@ -119,16 +99,16 @@ var catchPromiseErrors = function(done) {
   describe('Crawlability', function () {
     it('should return a partial when requesting /?_escaped_fragment_=/api/angular.bind', function (done) {
       request(HOST + '/?_escaped_fragment_=/api/angular.bind', function (err, res, body) {
-        expect(body).to.contain('Returns a function which calls function');
-        expect(body).to.contain('angular.bind')
+        expect(body).toContain('Returns a function which calls function');
+        expect(body).toContain('angular.bind')
         done();
       });
     });
 
     it('should return a partial when requesting /?_escaped_fragment_=/api/ng.directive:ngMouseover', function (done) {
       request(HOST + '/?_escaped_fragment_=/api/ng.directive:ngMouseover', function (err, res, body) {
-        expect(body).to.contain('Specify custom behavior on mouseover event.');
-        expect(body).to.contain('ngMouseover')
+        expect(body).toContain('Specify custom behavior on mouseover event.');
+        expect(body).toContain('ngMouseover')
         done();
       });
     });
@@ -137,7 +117,7 @@ var catchPromiseErrors = function(done) {
       it('should return an appropriate title when path contains : ', function (done) {
         request(HOST + '?_escaped_fragment_=/api/ng.directive:ngRepeat', function (err, res, body) {
           var title = /<title>(.*)<\/title>/.exec(body)[1];
-          expect(title).to.contain('AngularJS Documentation for ngRepeat');
+          expect(title).toContain('AngularJS Documentation for ngRepeat');
           done();
         });
       });
@@ -145,7 +125,7 @@ var catchPromiseErrors = function(done) {
       it('should return an appropriate title when path contains . ', function (done) {
         request(HOST + '/?_escaped_fragment_=/api/ng.$rootScope.Scope', function (err, res, body) {
           var title = /<title>(.*)<\/title>/.exec(body)[1];
-          expect(title).to.contain('AngularJS Documentation for Scope');
+          expect(title).toContain('AngularJS Documentation for Scope');
           done();
         });
       });
@@ -153,7 +133,7 @@ var catchPromiseErrors = function(done) {
       it('should return an appropriate title when path contains no special characters', function (done) {
         request(HOST + '?_escaped_fragment_=/api/ngSanitize', function (err, res, body) {
           var title = /<title>(.*)<\/title>/.exec(body)[1];
-          expect(title).to.contain('AngularJS Documentation for ngSanitize');
+          expect(title).toContain('AngularJS Documentation for ngSanitize');
           done();
         });
       });
@@ -161,63 +141,63 @@ var catchPromiseErrors = function(done) {
 
     it('should return a partial when requesting /api/ng.directive:ngHref?_escaped_fragment_', function (done) {
       request(HOST + '/api/ng.directive:ngHref?_escaped_fragment_', function (err, res, body) {
-        expect(body).to.contain('Using Angular markup like');
+        expect(body).toContain('Using Angular markup like');
         done();
       });
     });
 
     it('should return a partial when requesting /api/ng.directive:ngHref?_escaped_fragment_=', function (done) {
       request(HOST + '/api/ng.directive:ngHref?_escaped_fragment_=', function (err, res, body) {
-        expect(body).to.contain('Using Angular markup like');
+        expect(body).toContain('Using Angular markup like');
         done();
       });
     });
 
     it('should return a partial when requesting /guide/overview?_escaped_fragment_=', function (done) {
       request(HOST + '/guide/overview?_escaped_fragment_=', function (err, res, body) {
-        expect(body).to.contain('AngularJS is a structural framework for dynamic web apps');
+        expect(body).toContain('AngularJS is a structural framework for dynamic web apps');
         done();
       });
     });
 
     it('should return a partial when requesting /', function (done) {
       request(HOST + '?_escaped_fragment_=', function (err, res, body) {
-        expect(body).to.contain('Use the API Reference documentation when you need more information');
+        expect(body).toContain('Use the API Reference documentation when you need more information');
         done();
       });
     });
 
     it('should return a partial when requesting /guide?_escaped_fragment_=', function (done) {
       request(HOST + '/guide?_escaped_fragment_=', function (err, res, body) {
-        expect(body).to.contain('Welcome to the angular Developer Guide.');
+        expect(body).toContain('Welcome to the angular Developer Guide.');
         done();
       });
     });
 
     it('should return a partial when requesting /guide/?_escaped_fragment_=', function (done) {
       request(HOST + '/guide/?_escaped_fragment_=', function (err, res, body) {
-        expect(body).to.contain('Welcome to the angular Developer Guide.');
+        expect(body).toContain('Welcome to the angular Developer Guide.');
         done();
       });
     });
 
     it('should return a partial when requesting /cookbook/?_escaped_fragment_=', function (done) {
       request(HOST + '/cookbook/?_escaped_fragment_=', function (err, res, body) {
-        expect(body).to.contain('Welcome to the Angular cookbook.');
+        expect(body).toContain('Welcome to the Angular cookbook.');
         done();
       });
     });
 
     it('should return a partial when requesting /tutorial/?_escaped_fragment_=', function (done) {
       request(HOST + '/tutorial/?_escaped_fragment_=', function (err, res, body) {
-        expect(body).to.contain('A great way to get introduced to AngularJS is to work through this tutorial');
+        expect(body).toContain('A great way to get introduced to AngularJS is to work through this tutorial');
         done();
       });
     });
 
     it('should return a 404 status code for bogus _escaped_fragment_ urls', function (done) {
       request(HOST + '/misc/tutorial/api/guide/misc/misc/%7B%7Bmodule.url%7D%7D?_escaped_fragment_=', function (err, res, body) {
-        expect(res.statusCode).to.equal(404);
+        expect(res.statusCode).toEqual(404);
         done();
       });
     });
@@ -233,8 +213,8 @@ var catchPromiseErrors = function(done) {
           request(reqUrl, function (err, res, body) {
             finishedCount++;
 
-            expect(res.statusCode).to.equal(200);
-            expect(body).to.contain('API Reference');
+            expect(res.statusCode).toEqual(200);
+            expect(body).toContain('API Reference');
             
             if (queueCount === finishedCount) {
               done();
@@ -246,79 +226,54 @@ var catchPromiseErrors = function(done) {
   });
 
   describe('App', function () {
-    before(function (done) {
-      tractor.get(HOST);
-      done();
-    });
-
-    after(function (done) {
-      driver.quit().then(function () {
-        done();
-      });
-    });
-    
-    var search
-      , firstModule;
-
     it('should filter the module list when searching', function (done) {
-      search = tractor.findElement(protractor.By.input('search'));
+      tractor.get(HOST);
+      var search = tractor.findElement(protractor.By.input('search'));
       
       search.sendKeys('ngBind').then(function () {
-        firstModule = queryCss('[ng-repeat="page in module.directives"] a');
+        var firstModule = queryCss('[ng-repeat="page in module.directives"] a')
         firstModule.getText().then(function (text) {
-          expect(text).to.equal('ngBind');
+          expect(text).toEqual('ngBind');
           done();
         });
       });
     });
 
     it('should change the page content when clicking a module from the left', function (done) {
-      firstModule.click().then(function () {
+      tractor.get(HOST);
+      var ngBindLink = queryCss('a[href="api/ng.directive:ngBind"]')
+      ngBindLink.click().then(function () {
         //Wait for content to load
-        setTimeout(function (){
-          var pageBody = queryCss('.content h1 code');
-          pageBody.getText().then(function (text) {
-            expect(text).to.equal('ngBind');
-            done();
-          });
-        }, 500);
-        
+        var pageBody = queryCss('.content h1 code');
+        pageBody.getText().then(function (text) {
+          expect(text).toEqual('ngBind');
+          done();
+        });
       });
     });
 
-    //TODO: Implement test for prod only.
-    /*it('should load the Disqus comments', function (done) {
-
-      var disqusFrame = queryCss('#disqus iframe');
-      disqusFrame.getSize().then(function (size) {  
-        expect(size.height).to.be.greaterThan(10);
-        done();
-      });
-    });*/
-
     it('should show the functioning input directive example', function (done) {
       tractor.get(HOST + '/api/ng.directive:input');
-      // var nameInput = tractor.findElement(protractor.By.input('user.name'));
-      var nameInput = queryCss('[ng-model="user.name"]');
+      var nameInput = tractor.findElement(protractor.By.input('user.name'));
       nameInput.click();
       nameInput.sendKeys('!!!');
       nameInput.getAttribute('type').then(function (value) {
         var code = queryCss('.doc-example-live tt');
         code.getText().then(function (text) {
-          expect(text).to.contain('guest!!!');
+          expect(text).toContain('guest!!!');
           done();
         });
       })
-      
     });
 
     it('should show the functioning radio directive example', function (done) {
       tractor.get(HOST + '/api/ng.directive:input.radio');
       var redBtn = queryCss('form[name="myForm"] input[value="red"]');
+
       redBtn.click();
       var liveCode = queryCss('.doc-example-live tt');
       liveCode.getText().then(function (text) {
-        expect(text).to.contain('color = red');
+        expect(text).toContain('color = red');
         done();
       });
     });
