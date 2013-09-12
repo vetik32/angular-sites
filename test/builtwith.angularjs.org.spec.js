5cb6f705dcb6a0a3923da8d3696f72b0f1e9ac5d
Jeff Cross <middlefloor@gmail.com>
2013-09-12 13:53:20 +0200

    Updated builtwith tests to latest protractor.

--- test/builtwith.angularjs.org.spec.js
+++ test/builtwith.angularjs.org.spec.js
@@ -18,92 +18,75 @@ describe('builtwith.angularjs.org', function () {
     tractor.get(HOST);
   });
 
+
   describe('Rewrite', function () {
-    it('should rewrite /project/X to index.html', function (done) {
+    it('should rewrite /project/X to index.html', function () {
       request(HOST + '/project/YouTube-on-PS3', function (err, res, body) {
         expect(body).toContain('Built with AngularJS');
         expect(body).toContain('Super-powered by Google');
-        done();
       });
     });
   });
 
+
   describe('App', function () {
     describe('Homepage', function () {
-      it('should attach a scope to <body>', function (done) {
+      it('should attach a scope to <body>', function () {
         var body = tractor.findElement(protractor.By.css('body.ng-scope'));
-        expect(body).toBeTruthy();
-        done();
+        expect(body.getAttribute('class')).toContain('ng-scope');
       });
 
-      it('should allow searching of projects', function (done) {
+
+      it('should allow searching of projects', function () {
         var search = tractor.findElement(protractor.By.input('query'));
-        search.click().then(function () {
-          search.sendKeys('YouTube on PS3').then(function () {
-            var result1 = tractor.findElement(protractor.By.css('bwa-project > div > h2'));
-            result1.getText().then(function (text) {
-              expect(text).toEqual('YouTube on PS3');
-              search.clear();
-              done();
-            });
-          });
-        })
+        search.click()
+        search.sendKeys('YouTube on PS3')
+
+        var result1 = tractor.findElement(protractor.By.css('bwa-project > div > h2'));
+
+        expect(result1.getText()).toEqual('YouTube on PS3');
+        search.clear();
       });
 
-      it('should have a navbar with links in it', function (done) {
+
+      it('should have a navbar with links in it', function () {
         var homeBtn = tractor.findElement(protractor.By.css('.navbar .nav>li.active > a'));
-        homeBtn.getAttribute('href').then(function (href) {
-          expect(href).toEqual('http://angularjs.org/');
-          done();
-        });
+        expect(homeBtn.getAttribute('href')).toEqual('http://angularjs.org/');
       });
 
-      it('should have proper fields for the featured project', function (done) {
+
+      it('should have proper fields for the featured project', function () {
         var parentCSS = '[project="featured"]'
         var heroImg = tractor.findElement(protractor.By.css(parentCSS + ' .bwa-img'));
         var heroHeading = tractor.findElement(protractor.By.css(parentCSS + ' h2'));
         var heroDesc = tractor.findElement(protractor.By.css(parentCSS + ' .bwa-project-desc'));
         var heroTag = tractor.findElement(protractor.By.css(parentCSS + ' .label'));
 
-        heroImg.getAttribute('src').then(function (src) {
-          expect(src).toBeTruthy();
-
-          heroHeading.getText().then(function (text) {
-            expect(text).toBeTruthy();
-
-            heroDesc.getText().then(function (text) {
-              expect(text).toBeTruthy();
-
-              heroTag.getText().then(function (text) {
-                expect(text).toBeTruthy();
-                done();
-              })
-            })
-          })
-        });
+        expect(heroImg.getAttribute('src')).toBeTruthy();
+        expect(heroHeading.getText()).toBeTruthy();
+        expect(heroDesc.getText()).toBeTruthy();
+        expect(heroTag.getText()).toBeTruthy();
       });
 
-      it('should show a lightbox when clicking a project link', function (done) {
+
+      it('should show a lightbox when clicking a project link', function () {
         var projectLink = tractor.findElement(protractor.By.css('[project="featured"] > div[ng-click]'));
-        projectLink.click().then(function () {
-          var lightbox = tractor.findElement(protractor.By.css('.modal'));
-          lightbox.getCssValue('display').then(function (display) {
-            expect(display).toEqual('block');
-            done();
-          });
-        });
+        projectLink.click()
+
+        var lightbox = tractor.findElement(protractor.By.css('.modal'));
+        expect(lightbox.getCssValue('display')).toEqual('block');
       });
 
-      it('should show a count of all projects greater than 0', function (done) {
+
+      it('should show a count of all projects greater than 0', function () {
         var count = tractor.findElement(protractor.By.css('.bwa-count'));
-        count.getText().then(function (text) {
-          expect(parseInt(text, 10)).toBeGreaterThan(0);
-          done();
+        count.getText().then(function (num) {
+          expect(parseInt(num, 10)).toBeGreaterThan(0);
         });
       });
 
-      it('should sort projects alphabetically by name when name sort is selected', function (done) {
-        tractor.get(HOST);
+
+      it('should sort projects alphabetically by name when name sort is selected', function () {
         var option
           , names = []
           , prev
@@ -111,23 +94,22 @@ describe('builtwith.angularjs.org', function () {
           , completed = 0;
 
         option = tractor.findElement(protractor.By.css('[ng-model="sortPrep"] option[value="1"]'));
-        option.click().then(function () {
-          //Check order of first four items.
-          tractor.findElements(protractor.By.css('[ng-repeat="project in projectCol"] h2')).then(function (headings) {
-            headings.forEach(function (element, i) {
-              element.getText().then(function (text) {
-                if (typeof prev !== 'undefined' && text[0].toLowerCase() < prev) {
-                  ordered = false;
-                }
+        option.click();
 
-                prev = text[0].toLowerCase();
-                completed++;
+        //Check order of first four items.
+        tractor.findElements(protractor.By.css('[ng-repeat="project in projectCol"] h2')).then(function (headings) {
+          headings.forEach(function (element, i) {
+            element.getText().then(function (text) {
+              if (typeof prev !== 'undefined' && text[0].toLowerCase() < prev) {
+                ordered = false;
+              }
 
-                if (completed === headings.length) {
-                  expect(ordered).toBe(true);
-                  done();
-                }
-              });
+              prev = text[0].toLowerCase();
+              completed++;
+
+              if (completed === headings.length) {
+                expect(ordered).toBe(true);
+              }
             });
           });
         });
